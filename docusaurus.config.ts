10f6cb74afac6aef133a3345d5069e9a77a75782
Adrian Escutia <adrian@escutia.me>
2025-11-24 11:48:55 -0600

    post: OpenAI SDK and MCP integration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,8 +89,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/make-mcp-hapi.png',
     colorMode: {
       respectPrefersColorScheme: true,
       defaultMode: 'light',
